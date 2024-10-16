c9b80a24928c0e8c2cbf6240f072a0bd911b235d
josephbarnett <joe.barnett@cloudzero.com>
2024-10-15 22:46:49 -0300

    fix config loader

--- config/loader.go
+++ config/loader.go
@@ -9,18 +9,18 @@ type Configurable interface {
 }
 
 var (
-	mu   = sync.Mutex{}
-	cfgs = make([]Configurable, 0)
+	mu             = sync.Mutex{}
+	configurations = make([]Configurable, 0)
 )
 
 func Register(cfgs ...Configurable) {
 	mu.Lock()
 	defer mu.Unlock()
-	cfgs = append(cfgs, cfgs...)
+	configurations = append(configurations, cfgs...)
 }
 
 func Load() error {
-	for _, cfg := range cfgs {
+	for _, cfg := range configurations {
 		if err := cfg.Load(); err != nil {
 			return err
 		}
