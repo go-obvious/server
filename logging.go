94f6c141629c68b480c25f831ada9ba48c1f8860
josephbarnett <joe.barnett@cloudzero.com>
2025-04-22 22:35:21 -0400

    expose connection log filter

--- logging.go
+++ logging.go
@@ -11,9 +11,17 @@ type logAdapter struct {
 	io.Writer
 }
 
-type logFilter func(string) bool
+// ConnectionLogFilter defines a function type that takes a string as input
+// and returns a boolean. It is used to filter connection logs based on
+// specific criteria, where the input string typically represents a log entry
+// or a related identifier.
+type ConnectionLogFilter func(string) bool
 
-var filters = []logFilter{
+// filters is a slice of logFilter functions used to filter out specific log messages
+// related to TLS handshake errors. These filters are designed to ignore known,
+// non-critical errors that occur during TLS handshakes, such as aborted connections
+// or resets, which are expected behavior in certain scenarios.
+var filters = []ConnectionLogFilter{
 	func(s string) bool {
 		// First filter: Swallow specific TLS handshake errors
 		// On the Go issue tracker, users repeatedly report that random scanners, health‑checks or browsers will open
@@ -23,7 +31,18 @@ var filters = []logFilter{
 		// Start here: https://github.com/golang/go/issues/56382
 		return strings.Contains(s, "http: TLS handshake error") && strings.HasSuffix(s, "EOF\n")
 	},
-	// Add more filters here as needed
+	func(s string) bool {
+		// This filter ignores log messages containing "http: TLS handshake error"
+		// with "read: connection reset by peer". This is to handle cases where the connection
+		// is reset by the client during the handshake process.
+		return strings.Contains(s, "http: TLS handshake error") && strings.Contains(s, "read: connection reset by peer")
+	},
+}
+
+// AddFilter allows adding a new logFilter to the filters slice.
+// This can be used to dynamically add filters for log messages to ignore.
+func AddFilter(filter ConnectionLogFilter) {
+	filters = append(filters, filter)
 }
 
 func (f logAdapter) Write(p []byte) (int, error) {
